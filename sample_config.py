1ab51c7e246c4ad92104a7fa3e2cd45a3f78e387
TheHamkerCat <thehamkercat@gmail.com>
2021-03-14 11:18:48 +0530

    Changed API

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
 api_id = 69420
 api_hash = "854qa6e5rfawse6f7a6w8e7a74f63sd57f6we"
 bot_token = "3as4f65saf:fa5s7fa5w7f5877sf5aw6e8r7fa6"
-ARQ_API_BASE_URL = "http://52.0.6.104:8000"
+ARQ_API_BASE_URL = "https://thearq.tech/"
